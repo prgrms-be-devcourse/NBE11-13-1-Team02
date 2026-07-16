1f94a5aa0fab4127b2cded3146574ed08dc8c2a6
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-16 11:15:37 +0900

    first commit

---
+++ nbe11-13-1-team2/src/main/java/com/example/nbe11131team2/Nbe11131Team2Application.java
@@ -0,0 +1,13 @@
+package com.example.nbe11131team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe11131Team2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe11131Team2Application.class, args);
+    }
+
+}
