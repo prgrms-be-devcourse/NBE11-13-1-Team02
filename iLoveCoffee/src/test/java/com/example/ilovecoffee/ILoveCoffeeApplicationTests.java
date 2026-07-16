c5af4a4d759ebf8c8bc6096d91d29be800a75edc
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-16 11:34:47 +0900

    first commit

---
+++ iLoveCoffee/src/test/java/com/example/ilovecoffee/ILoveCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ilovecoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILoveCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
