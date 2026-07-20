68e30831396dcb5b02eab612f298dce40cacf6b9
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-20 10:21:55 +0900

    readme

--- iLoveCoffee/src/main/java/com/example/ilovecoffee/domain/enums/OrderStatus.java
+++ iLoveCoffee/src/main/java/com/example/ilovecoffee/domain/enums/OrderStatus.java
@@ -6,13 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OrderStatus {
+    PENDING("주문 대기 중"),
     CONFIRMED("주문 확정"),
-    PAID("결제 완료"),
     PREPARING("상품 준비 중"),
-    COMPLETED("상품 배송 완료"),
     DISPATCHED("출고 완료"),
-    CANCELED("주문 취소"),
-    REFUNDED("환불 완료");
+    COMPLETED("상품 배송 완료");
+
 
     private final String comment;
 }
